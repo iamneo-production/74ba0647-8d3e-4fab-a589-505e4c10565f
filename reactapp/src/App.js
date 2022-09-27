f69c6e62197e616c065a7fb69ef31979da9dd57b
bojadlabalaji <bojadlabalaji@gmail.com>
2022-09-27 10:34:32 +0000

    initial commit(springboot)

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,18 @@
-
+import Register from "./pages/registation";
 import './App.css';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        Hello Virtusa
+       <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+          
+               <Register/>
+          </Route>
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
