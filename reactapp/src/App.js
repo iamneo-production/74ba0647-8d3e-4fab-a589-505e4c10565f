78493c49ad7bf05d7ad537e6497d3e2a016a2696
bojadlabalaji <bojadlabalaji@gmail.com>
2022-09-28 06:48:04 +0000

    spring added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,10 +3,15 @@ import User from "./pages/User/User";
 import Auth from "./pages/Auth";
 import './App.css';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
-import {Link} from 'react-router-dom';
+import  LoginPage from './pages/LoginPage';
+import { Dashboard } from './pages/dashboard/dashboard';
+
+
 function App() {
   return (
     <div className="App">
+      <Route exact path="/" component={LoginPage}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
        <BrowserRouter>
         <Switch>
         <Route path="/user" exact>
---
+++ springapp/src/main/java/com/examly/springapp/repository/UserDetailsRepository.java
@@ -0,0 +1,14 @@
+package com.examly.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.examly.springapp.entities.User;
+
+
+@Repository
+public interface UserDetailsRepository extends JpaRepository<User, Long> {
+
+	User findByUserName(String userName);
+	
+}
